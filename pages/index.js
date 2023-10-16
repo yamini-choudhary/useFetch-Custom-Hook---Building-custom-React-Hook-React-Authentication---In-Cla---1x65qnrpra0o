04f5ec81b6a6facaabbc6642f15c79516be2fc69
yamini-choudhary <120270758+yamini-choudhary@users.noreply.github.com>
2023-10-16 20:25:19 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import useFetch from '../components/useFetch';
+import { useFetch } from '../components/useFetch';
 
 function Home() {
   const { data, loading, error } = useFetch("https://content.newtonschool.co/v1/pr/63b6c911af4f30335b4b3b89/products?limit=10&page=1");
@@ -10,12 +10,10 @@ function Home() {
   return (
     <div>
       <h1>Products</h1>
-      {/* {document.write(data)} */}
       <ul>
-      {data && data.map(item => (
-        <li key={item.id}>{item.title}</li>
-      ))}
-
+        {data && data.map(item => (
+          <li key={item.id}>{item.title}</li>
+        ))}
       </ul>
     </div>
   );
