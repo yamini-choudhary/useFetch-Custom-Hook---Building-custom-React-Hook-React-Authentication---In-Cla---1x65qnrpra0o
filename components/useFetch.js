39a3314f3ff77d43d95c7d27dd35aefff98df72b
yamini-choudhary <120270758+yamini-choudhary@users.noreply.github.com>
2023-10-16 20:26:39 +0530

    Update useFetch.js

--- components/useFetch.js
+++ components/useFetch.js
@@ -1,3 +1,28 @@
-//write your code here
+import { useState, useEffect } from 'react';
 
-export default useFetch
+export function useFetch(url) {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const result = await response.json();
+        setData(result);
+        setLoading(false);
+      } catch (err) {
+        setError(err.message);
+        setLoading(false);
+      }
+    }
+
+    fetchData();
+  }, [url]);
+
+  return { data, loading, error };
+}
